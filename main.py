c00db9e8e360a557b37369d142f81479f44117b2
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-01-16 01:31:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+
+####################################
+#Write your code below this line 👇
+# The a,b, and c variable represent the 1st, 2nd, and combination of the two.
+a=str(two_digit_number[0])
+b=str(two_digit_number[1])
+c=(int(a)+int(b))
+print(c)
+
+
+
+
+
+
